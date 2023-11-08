79b12209551aa0395a504b23e3b04e8259554a34
fabien chareun <cacrfabfr@gmail.com>
2023-11-08 23:17:43 +0100

    colors + grid

--- src/components/weaponItem/WeaponItem.jsx
+++ src/components/weaponItem/WeaponItem.jsx
@@ -6,35 +6,40 @@ const WeaponItem = ({weapon, handleMasteryProgressClick, category}) => {
         <h3>{weapon.name}</h3>
 
         {/* Multiplayer camos */}
-        {Object.keys(weapon.masteryProgress).map((progressKey) => (
-            <div
-                key={progressKey}
-                className={`camo`}
-                onClick={() =>
-                    handleMasteryProgressClick(category, weapon.name, progressKey)
-                }
-            >
-              <div className={`${progressKey} camoPic ${weapon.masteryProgress[progressKey] ? 'active' : ''}`}>
+        <div className="multiplayer_container">
+          {Object.keys(weapon.masteryProgress).map((progressKey) => (
+              <div
+                  key={progressKey}
+                  className={`camo`}
+                  onClick={() =>
+                      handleMasteryProgressClick(category, weapon.name, progressKey)
+                  }
+              >
+                <div className={`${progressKey} camoPic ${weapon.masteryProgress[progressKey] ? 'active' : ''}`}>
+                </div>
               </div>
-            </div>
-        ))}
+          ))}
+        </div>
 
         {/* Multi / Zombie separator gradient */}
         <div className="separator" />
 
-        {/* Zombie camos */}
-        {Object.keys(weapon.masteryProgressZombie).map((progressKey) => (
-            <div
-                key={progressKey}
-                className={'camo zombie'}
-                onClick={() =>
-                    handleMasteryProgressClick(category, weapon.name, progressKey, true)
-                }
-            >
-              <div className={`${progressKey} camoPic ${weapon.masteryProgressZombie[progressKey] ? 'active' : ''}`}>
+        <div className="zombies_container">
+          {/* Zombie camos */}
+          {Object.keys(weapon.masteryProgressZombie).map((progressKey) => (
+              <div
+                  key={progressKey}
+                  className={'camo zombie'}
+                  onClick={() =>
+                      handleMasteryProgressClick(category, weapon.name, progressKey, true)
+                  }
+              >
+                <div className={`${progressKey} camoPic ${weapon.masteryProgressZombie[progressKey] ? 'active' : ''}`}>
+                </div>
               </div>
-            </div>
-        ))}
+          ))}
+        </div>
+
       </li>
   );
 };
